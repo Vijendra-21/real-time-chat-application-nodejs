8ed3a84c1d36b3dc8b52bdf4192fb5c7088487de
goraji <gorasunil61@gmail.com>
2021-05-12 22:32:58 +0530

    publish

---
+++ client.js
@@ -0,0 +1,45 @@
+const socket = io('http://localhost:5000');
+let userName ;
+let messageInp = document.querySelector('#messageInp');
+let messageArea = document.querySelector('.messageArea')
+do {
+    userName = prompt(`plz enter your name: `)
+} while (!userName)
+
+
+socket.on('recieve',(data)=>{
+    append(`${data.name}: ${data.message}`,'left')
+    scrollToBottom();
+})
+
+
+socket.emit('new-user-joined',userName)
+socket.on('user-joined',(user)=>{
+    append(`${user} joined!!!`,'center')
+    scrollToBottom();
+}) 
+
+socket.on('left',(names)=>{
+    append(`${names} left!!!`,'center')
+})
+
+function append(msg,position){
+    let mainDiv = document.createElement('div');
+    mainDiv.innerText = msg;
+    mainDiv.classList.add('message')
+    mainDiv.classList.add(position)
+    messageArea.append(mainDiv)
+    scrollToBottom();
+}
+messageInp.addEventListener('keyup',(e)=>{
+    if(e.key === 'Enter'){
+        let msg = messageInp.value;
+        append(`You :${msg}`,'right');
+        messageInp.value = ''
+        socket.emit('send',msg);  
+    }
+})
+function scrollToBottom (){
+    messageArea.scrollTop = messageArea.scrollHeight; 
+}
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app); 
+const PORT = proces.env.PORT || 5000;
+
+const staticPath = path.join(__dirname,'/public')
+app.use(express.static(staticPath));
+
+app.get('/',(req,res)=>{
+    res.send('index.html')
+})
+
+http.listen(PORT,()=>{
+   console.log(`server is running at ${PORT}`)
+});
+
+const  io = require("socket.io")(http);
+
+const users = {};
+
+io.on('connection',(socket)=>{
+    socket.on('send',(msg)=>{
+        socket.broadcast.emit('recieve',{message:msg , name:users[socket.id]})
+    })
+    socket.on('new-user-joined',(name)=>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name)
+    })
+    socket.on('disconnect',(msg)=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    })
+})
+
